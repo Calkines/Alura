1ef238ee5aa7888d2cbd6ada3ed0bb3ed1bf085a
Rafael <rafael_it@hotmail.com>
2019-02-10 16:55:19 -0200

    Incluindo lição 1 - Colocando o NHibernate para funcionar

---
+++ NHibernate/Lesson01-Loja/Alura/Loja/Infra/NHibernateHelper.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Loja.Infra {
+    public class NHibernateHelper {
+
+        public static Configuration GetConfiguracao() {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            return cfg;
+        }
+
+        public static void SetSchema() {
+            Configuration cfg = GetConfiguracao();
+            new SchemaExport(cfg).Create(true, true);
+        }
+    }    
+}
---
+++ NHibernate/Lesson01-Loja/Alura/Loja/Program.cs
@@ -0,0 +1,18 @@
+﻿using Loja.Infra;
+using NHibernate.Cfg;
+using System;
+using System.Reflection;
+
+namespace Loja {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            //var cfg = NHibernateHelper.GetConfiguracao();
+            //var session = cfg.BuildSessionFactory();
+            //session.OpenSession();
+            NHibernateHelper.SetSchema();
+            Console.ReadLine();
+        }
+    }
+}
